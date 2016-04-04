4ea4f73dfcc1baab22d463f943ce7f10250810a1
Zach <zgiles@gmail.com>
2016-04-04 18:08:51 -0400

    Beautify the code, and a missing comma

--- app/pages/App.jsx
+++ app/pages/App.jsx
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 export default class App extends Component {
 
   // change something in here to see hotloading work. Either example should work
 
   // This is the canonical example..
-	/*
+  /*
   render() {
     return (
       <h1>Hello, world.</h1>
@@ -15,9 +15,10 @@ export default class App extends Component {
 
   // the following is silly, but demo's ES6...
   render() {
-  	return (  () => { return (
-			<h1>Hi World</h1>
-			); }
-	        )()
+    return (() => {
+      return (
+        <h1>Hi World</h1>
+      );
+    })()
   }
 }
--- webpack.prod.js
+++ webpack.prod.js
@@ -4,41 +4,39 @@ var ROOT_PATH = path.resolve(__dirname);
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: [ 
-	    path.resolve(ROOT_PATH, 'app'),
-	   ],
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    },
-    output: {
-        path: path.resolve(ROOT_PATH, 'dist'),
-        filename: 'bundle.js',
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-      	template: path.resolve(ROOT_PATH, 'app/index.html'),
-      	inject: true
-      }),
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          warnings: false
-        }
-      }),
-      new webpack.DefinePlugin({
-        'process.env': {
+  entry: [
+    path.resolve(ROOT_PATH, 'app'),
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: path.resolve(ROOT_PATH, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(ROOT_PATH, 'app/index.html'),
+      inject: true
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
         'NODE_ENV': JSON.stringify('production')
-        }
-      })
-    ],
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-		exclude: /(node_modules|bower_components)/,
-		loaders: [ "react-hot", "babel-loader" ],
-                include: path.resolve(ROOT_PATH, 'app')
-            }
-        ]
-    }
+      }
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loaders: ["react-hot", "babel-loader"],
+      include: path.resolve(ROOT_PATH, 'app')
+    }]
+  }
 };
